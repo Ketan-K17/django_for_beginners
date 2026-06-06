fa18c0cbec07c5874512c092f2397ab35220194a
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-06 20:17:22 +0530

    Learnt about named urls, and how we must use it to avoid hardcoding paths across codebase

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import home_page_view, AboutPageView
 
 urlpatterns = [
-    path("", home_page_view),
-    path("about/", AboutPageView.as_view()),
+    path("", home_page_view, name="home"), # ALWAYS name your urls.. This way you don't need to hardcode path everywhere... if url changes tomorrow for some reason, only place to edit would be urls.py file.
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
