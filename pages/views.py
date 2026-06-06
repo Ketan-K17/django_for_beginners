47b592aa4a6060f969d2cfc9bca486cc6103cb92
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-06 17:35:07 +0530

    Learnt about Templates... 1. how to store them within an app, and why in that fashion. 2. how to render templates, what's 'context', how to pass variables from view to the template through context 3. Learnt about django templating library's 'comment' tags, and why you need to use them over regular html comment tags.

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 # A view is a Python function that accepts a Web request and returns a Web response. 
 
-# pages/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page_view(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("HomePage")
+
+def about_page_view(request):
+    return render(request, "pages/about.html", context={})
+    # NOTE: Templates often have some dynamic content (not HTML) that is to be shown on the final rendered HTML. These could include values picked from a database or other sources. All of these values to be shown on the final rendered HTML must be passed to the template as 'context' data. 'context' is a dictionary of key-value pairs that are used to pass data to the template. You use it as the last of 3 params to pass to the render() function..
+
+    # so a render function needs 1. httprequest obj, 2. template name, 3. context data
