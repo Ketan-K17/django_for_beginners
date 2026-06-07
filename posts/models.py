f933e930a28c59f3ab222321790795b71c38c647
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-07 16:31:35 +0530

    New app: Posts.
    1. learnt what models are, how changes to models are adopted into the actual database schema, what migration files do etc.

---
+++ posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    text = models.TextField()
+
+# This is our first change to the models file for this 'posts' app. EVERY change to the models file -> eventual change in database schema. This schema is actually stored to the db by a 2 step process. Here's what it looks like - 
+
+# create / change model file -> makemigrations (creates migration file) -> migrate (adopts this schema change in db)
+
+# this 2-step process aka 'activation' of model.
