411c0791de65f42d62f56a669e700813018c4ffd
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-07 16:07:29 +0530

    learnt a little bit more about writing tests..
    1. learnt of the reverse() mtd, used by the 'url' tag internally, helps resolve urls in python code.. Use it everywhere in your views / test file.
    2. learnt of assertContains / assertTemplateUsed
    3. Realized that the process of using reusable templates as a smaller part of other templates is called 'Template Inheritance'.

--- pages/tests.py
+++ pages/tests.py
@@ -1,18 +1,45 @@
-from django.test import TestCase, SimpleTestCase
+
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Company Homepage</h1>")
+
 class AboutpageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
 
-# NOTE: There are 4 types of unit tests in Django (these are extensions of the base unittest class in python)
-# 1. SimpleTestCase: for testing views that don't require a database (e.g. views that just return a static response)
-# 2. TestCase: for testing views that require a database (e.g. views that query the database)
-# 3. TransactionTestCase: for testing views that require a database (e.g. views that query the database) and need to be rolled back after the test is complete (e.g. to avoid side effects on the database)
-# 4. LiveServerTestCase: for testing views that require a live server (e.g. views that test the server's response)
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>Company About Page</h1>")
+
+# NOTE: What's the reverse() method? 
+# reverse() is a Django function that returns the URL for a given view name. It is used to generate URLs for views in your templates. Basically the equivalent of the 'url' tag in python. Use this to further not have to hardcode urls in your view / test files..
+
+# reverse("about") → "/about/"
+# {% url "about" %} → calls reverse("about") internally → "/about/"
+
+# NOTE: assertContains() and assertTemplateUsed() are Django test assertions that check if a response contains a specific string or template is used. Use this to be absolutely sure the right template is being accessed.
