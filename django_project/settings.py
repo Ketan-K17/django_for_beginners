67fc669b3b8d02fa031516e9fbafd6f044dea7e7
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-07 16:43:53 +0530

    got django-jazzmin ui package to pimp up the admin page

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
     'pages',
     'posts',
 ]
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "lux"
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
