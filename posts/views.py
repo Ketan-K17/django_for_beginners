32da4861e5c0d7d9604f732529e6789e55789568
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-15 12:20:10 +0530

    Built and tested our first database-driven app and learned how to create a database model, update it with the admin panel, and write tests. We also looked at both function-based and class-based approaches for the views

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from .models import Post
-from django.urls import reverse
+# from django.shortcuts import render
+# from .models import Post
+# from django.urls import reverse
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "posts.html", {"posts": posts})
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, "posts.html", {"posts": posts})
+
+from django.views.generic import ListView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    template_name = "posts.html"
