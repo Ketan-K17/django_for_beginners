ad8897ea9cade3dc3bd2255cef7edc574f262be2
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-06-28 10:28:02 +0530

    Implemented article management features in the newspaper app
    1. Added Article model with fields for title, body, date, and author.
    2. Created admin interface for Article management.
    3. Updated URLs to include article list, detail, create, update, and delete views.
    4. Developed views for listing, creating, updating, and deleting articles, ensuring user authentication for article creation.
    5. Enhanced home template to display articles with links for editing and deleting.
    6. Updated base template for improved navigation to article creation.

--- newspaper_app/views.py
+++ newspaper_app/views.py
@@ -1,4 +1,42 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
+from django.urls import reverse_lazy
+from .models import Article
 
-class HomePageView(TemplateView):
+
+class ArticleListView(ListView):
+    model = Article
     template_name = "home.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ["title", "body"]
+    template_name = "article_edit.html"
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("home")
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    fields = ["title", "body"]
+    template_name = "article_new.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
